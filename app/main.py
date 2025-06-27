f3a809f500929ea9adf0881bf5ffb89408bdab6f
Otstoy <otstoy.sxe@gmail.com>
2025-06-27 13:18:38 +0300

    Refactored indentations if __init__ method of Dog class.

--- app/main.py
+++ app/main.py
@@ -40,9 +40,9 @@ class Cat(Animal):
 class Dog(Animal):
 
     def __init__(
-            self, name:
-            str, is_hungry:
-            bool = True
+            self,
+            name: str,
+            is_hungry: bool = True
     ) -> None:
 
         super().__init__(name, 7, is_hungry)
